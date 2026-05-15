5c416f95f50100cd40b31aa710df2b07d8902c03
ArranTuksUni <u23655675@tuks.co.za>
2026-05-15 13:59:08 +0200

    created dropdown.test.tsx and imported the required modules for the test driven development of the component

---
+++ frontend/__tests__/components/ui/dropdown.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Dropdown from '../../../src/components/ui/dropdown';
+
+const mockOnChange = jest.fn();
+
