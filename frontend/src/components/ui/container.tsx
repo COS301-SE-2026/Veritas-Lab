643718641bbdd7f307b1a24693c2c25be2668012
ArranTuksUni <u23655675@tuks.co.za>
2026-05-16 10:29:39 +0200

    completed initial functionality for container.tsx component

--- frontend/src/components/ui/container.tsx
+++ frontend/src/components/ui/container.tsx
@@ -4,3 +4,10 @@ type ContainerProps = {
 	children: ReactNode;
 	className?: string;
 };
+
+//creation of Container comp
+export default function Container({ children, className }: ContainerProps)
+{
+	return <div className={className}>{children}</div>;
+}
+//we may eventually need to add more but for now this seems to be all we need (depending on if we decide to use a global style file)
