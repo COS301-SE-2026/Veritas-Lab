1edd1b66a4478b6262260cdedba732f22b5705da
Keegan-l-star <u24594522@tuks.co.za>
2026-05-16 20:11:53 +0200

    added some stubs for the cases so that Tshepiso can do the functionality

--- Backend/app/core/cases.py
+++ Backend/app/core/cases.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
+from datetime import datetime
+import asyncpg
+
 class Case:
-    """Placeholder Case class - tests will fail until implemented"""
-    pass
+    
+    def __init__(self, CaseCreator: str = None, CaseName: str = None, CaseReviews: dict = None):
+        pass
+    
+    async def save(self):
+        pass
