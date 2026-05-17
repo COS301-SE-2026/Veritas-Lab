8f149c359811557f7b110648c3d51f85d112c465
ArranTuksUni <u23655675@tuks.co.za>
2026-05-17 14:52:20 +0200

    created loginForm.test.tsx and imported the required modules for the testing of the component

---
+++ frontend/__tests__/components/common/loginForm.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import LoginForm from '../../../src/components/common/loginForm';
+
+const mockPush = jest.fn();
