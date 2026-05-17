f4db712547db2b7f114af33dd13181f3ca1560fa
CodeMakelana <u23588579@tuks.co.za>
2026-05-17 16:14:54 +0200

    update error message for CaseCreator length validation

--- Backend/app/core/cases.py
+++ Backend/app/core/cases.py
@@ -11,7 +11,7 @@ class Case:
         if len(CaseName) > 255:
             raise ValueError("CaseName must be 255 characters or less")
         if len(CaseCreator) > 100:
-            raise ValueError("CaseCreator must be 100 characters or less")
+            raise ValueError("Name is too long. Must be 100 characters or less")
 
         self.CaseId = str(uuid4())
         self.CaseCreator = CaseCreator
