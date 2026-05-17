9e42343ed80a0026c0812fff01c4db1895fe905d
CodeMakelana <u23588579@tuks.co.za>
2026-05-17 16:13:46 +0200

    add cases router to main API setup

--- Backend/app/api/main.py
+++ Backend/app/api/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 import re
 from pydantic import BaseModel # For JSON
 from app.auth.auth import router as auth_router
+from app.api.routers.cases_router import router as cases_router
 
 app = FastAPI(
     title="Veritas Lab API",
     description="This is the backend REST API for Veritas Lab"
 )
+
+app.include_router(cases_router)
 
 app.include_router(auth_router)
 
