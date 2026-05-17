d5ab6366ab4698d964e8c86c3652ff5296f9e6ff
Mulondi-25 <makhadomulondi@gmail.com>
2026-05-17 23:05:25 +0200

    case management modal

---
+++ frontend/src/components/common/caseManagementModal.tsx
@@ -0,0 +1,29 @@
+import Modal from "../ui/modal";
+import Button from "../ui/button";
+import Label from "../ui/label";
+import Input from "../ui/input";
+type CaseManagementModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+};
+export default function CaseManagementModal({ isOpen, onClose }: CaseManagementModalProps) {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <form>
+                <div className="text-[24px] font-bold text-[#231F20] mb-4">Create New Case</div>
+                <Label text="Case Title" htmlFor="caseTitle" className="mb-2 text-[16px] text-[#231F20]" />
+                <Input id="caseTitle" type="text" placeholder="Enter case title" className="border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-[#3DBF79] mb-4 w-full text-[16px] text-[#231F20]" required />
+                <Label text="Case Description" htmlFor="caseDescription" className="mb-2 text-[16px] text-[#231F20]" />
+                <Input id="caseDescription" type="text" placeholder="Enter case description" className="border border-gray-300 rounded-lg py-10 px-4 focus:outline-none focus:ring-2 focus:ring-[#3DBF79] mb-4 w-full text-[16px] text-[#231F20]" required />
+                <div className="flex justify-end">
+                    <Button variant="outline" onClick={onClose} className="mr-2">
+                        <div className="text-[16px] font-bold">Cancel</div>
+                    </Button>  
+                    <Button variant="submit" onClick={onClose}>
+                        <div className="text-[16px] font-bold">Create Case</div>
+                    </Button>
+                </div>
+            </form>
+        </Modal>
+    );
+}
--- frontend/src/components/ui/modal.tsx
+++ frontend/src/components/ui/modal.tsx
@@ -13,7 +13,7 @@ export default function Modal({ children, isOpen, onClose }: ModalProps) {
     return (
         <>
         <div className="fixed inset-0 bg-black/50 flex justify-center items-center z-50" onClick={onClose}>
-            <div className="bg-white rounded-2xl p-8 w-full max-w-md shadow-lg">
+            <div className="bg-white rounded-2xl p-8 w-full max-w-md shadow-lg" onClick={(e) => e.stopPropagation()}>
                 {children}
             </div>
         </div>
