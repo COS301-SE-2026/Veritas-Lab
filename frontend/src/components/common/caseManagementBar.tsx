a8840c35a981a9853a4ab8efba271c949276a6a3
Mulondi-25 <makhadomulondi@gmail.com>
2026-05-18 21:53:50 +0200

    Slider bar component and re-styled caseManagementBar

--- frontend/src/components/common/caseManagementBar.tsx
+++ frontend/src/components/common/caseManagementBar.tsx
@@ -1,24 +1,22 @@
 'use client';
 import Input from '../ui/input';
 import Dropdown from '../ui/dropdown';
+import SliderBar from '../ui/sliderBar';
 export default function CaseManagementBar() {
     return (
         <>
-        <div className='grid grid-cols-3 gap-4 rounded-full bg-[#3DBF79] font-semibold border-2 p-4 mt-4'>
+        <div className='grid grid-cols-3 gap-4 rounded-full font-semibold text-[var(--color-text)] p-4 mt-4'>
             <div>
-                <Input placeholder="Search cases..." />
+                <Input placeholder="Search cases..." className='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] pl-5 w-full py-2.5 rounded-full'/>
             </div>
             <div>
-                <Dropdown options={[
-                    {'label': 'All Cases', 'value': 'all'}, 
-                    {'label': 'Open Cases', 'value': 'open'}, 
-                    {'label': 'Closed Cases', 'value': 'closed'}]} />
+                <SliderBar filters={['All', 'Open', 'Closed']}  />
             </div>
             <div>
                 <Dropdown options={[
                     {'label': 'Date Created', 'value': 'date_created'}, 
                     {'label': 'Last Updated', 'value': 'last_updated'}, 
-                    {'label': 'Priority', 'value': 'priority'}]} />
+                    {'label': 'Priority', 'value': 'priority'}]} className='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] pl-5 ml-3 w-full py-3.5 rounded-full' optionClassName='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] rounded-full'/>
             </div>
         </div>
         </>
