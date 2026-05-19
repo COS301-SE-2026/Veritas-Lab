22df93d565d81f7499972f98c3240ac184f0aff7
Keegan-l-star <u24594522@tuks.co.za>
2026-05-19 13:15:51 +0200

    Added something to the database

--- Backend/app/database/init_cases.sql
+++ Backend/app/database/init_cases.sql
@@ -1,7 +1,7 @@
-INSERT INTO "Cases_DB"."MediaType" (MediaName, MediaExtension) VALUES
-('Portable Network Graphics', '.png'),
-('Joint Photographic Experts Group', '.jpeg'),
-('JPEG Image', '.jpg'),
-('Portable Document Format', '.pdf')
+INSERT INTO "Cases_DB"."MediaType" (MediaName,MediaBucket, MediaExtension) VALUES
+('Portable Network Graphics','images', '.png'),
+('Joint Photographic Experts Group','images' '.jpeg'),
+('JPEG Image','images', '.jpg'),
+('Portable Document Format','pdf', '.pdf')
 ON CONFLICT (MediaName) DO NOTHING;
 
