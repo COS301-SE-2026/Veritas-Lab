a40c21c8987ed4616e20f0c2a97069402ea305c5
Keegan-l-star <u24594522@tuks.co.za>
2026-05-28 09:49:20 +0200

    Added a name to the report and handled the insertion during evidence submission

--- Backend/app/core/cases.py
+++ Backend/app/core/cases.py
@@ -147,7 +147,6 @@ class Case:
             if existingMedia:
                 mediaId=existingMedia["MediaId"]
                 targetFilename = f"{mediaId}{dbExtension}"
-                # Allow the dupe we just won't save it
             else: 
                 newMediaUuid = uuid.uuid4()
 
@@ -174,17 +173,26 @@ class Case:
                 )  
 
             try:
+                // Insesrt into the Reports table allowing the report to have the image's name in the image title column
+                //
                 await connection.execute(
                     """
-                    INSERT INTO "Cases_DB"."Reports" (CaseId, ImageId, ReportArtifacts, ReportFindings, ReportComments)
+                    INSERT INTO "Cases_DB"."Reports" (CaseId, ImageId, ImageTitle, ReportArtifacts, ReportFindings, ReportComments)
                     VALUES ($1, $2, $3, $4, $5)
                     """,
                     case_uuid,
                     mediaId,
+                    filename,
                     None,
                     None,
                     None
                 )
+
+            except asyncpg.UniqueViolationError:
+                return {
+                    "Status": "duplicate_report - image already associated with this case",
+                    "MediaId": str(mediaId),
+                }
             except Exception:
                 pass
 
@@ -193,6 +201,7 @@ class Case:
 
             return{
                 "MediaId": str(mediaId),
+                "Filename": filename,
                 "url": fileUrl,
                 "Status": "existing" if existingMedia else "uploaded"
             }
