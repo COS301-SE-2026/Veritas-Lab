eee851e14adb88613c54389f068f64b4701378fb
Mulondi-25 <makhadomulondi@gmail.com>
2026-06-25 03:27:16 +0200

    Added fetching and deleting the jwt cookie

---
+++ frontend/src/auth/cookie.tsx
@@ -0,0 +1,18 @@
+'use server'
+
+//import { cookies } from "next/dist/server/request/cookies";
+import { cookies } from 'next/headers'
+
+export async function getCookie() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('JWT_token')?.value;
+  if (!token) {
+    return "";
+  }
+  return token;
+};
+
+export async function deleteCookie() {
+  const cookieStore = await cookies();
+  cookieStore.delete('JWT_token');
+};
