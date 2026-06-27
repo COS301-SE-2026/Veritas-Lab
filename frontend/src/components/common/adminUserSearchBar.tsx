ff039b71d2beba2891bb28219ce2504589ec53ca
ArranTuksUni <u23655675@tuks.co.za>
2026-06-28 01:58:47 +0200

    completed admin search bar component

--- frontend/src/components/common/adminUserSearchBar.tsx
+++ frontend/src/components/common/adminUserSearchBar.tsx
@@ -14,3 +14,50 @@ type AdminUserSearchBarProps = {
     sortOptions?: { label: string; value: string }[];
     onSortChange?: (value: string) => void;
 };
+//user search bar which is identical to the dsahboard basically.
+export default function AdminUserSearchBar({
+    searchValue,
+    onSearchChange,
+    searchPlaceholder = 'Search users...',
+    filters = ['All', 'ADMIN', 'INVESTIGATOR', 'USER'],
+    roleFilter,
+    onRoleChange,
+    sortValue,
+    sortOptions = [
+        { label: 'User Name', value: 'displayName' },
+        { label: 'Username', value: 'username' },
+        { label: 'User ID', value: 'id' },
+        { label: 'Role', value: 'role' },
+    ],
+    onSortChange,
+}: AdminUserSearchBarProps) {
+    return (
+        <div className='grid gap-4 rounded-full font-semibold text-[var(--color-text)] p-4 mt-4 md:grid-cols-3'>
+            <div>
+                <Input
+                    placeholder={searchPlaceholder}
+                    className='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] pl-5 w-full py-2.5 rounded-full'
+                    value={searchValue}
+                    onChange={onSearchChange}
+                />
+            </div>
+            <div>
+                <SliderBar
+                    filters={filters}
+                    className='w-full'
+                    defaultFilter={roleFilter}
+                    onChange={onRoleChange}
+                />
+            </div>
+            <div>
+                <Dropdown
+                    options={sortOptions}
+                    className='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] pl-5 w-full py-3.5 rounded-full'
+                    optionClassName='shadow-[inset_0_0_8px_rgba(0,0,0,0.1)] rounded-full'
+                    defaultValue={sortValue}
+                    onChange={(event) => onSortChange?.(event.target.value)}
+                />
+            </div>
+        </div>
+    );
+}
