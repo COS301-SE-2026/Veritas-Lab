e95756381a897c5c915ad650f1931bef7351d64f
Mulondi-25 <makhadomulondi@gmail.com>
2026-07-04 19:27:17 +0200

    Completed top half of landing page and added shapes

--- frontend/src/app/(auth)/landing/page.tsx
+++ frontend/src/app/(auth)/landing/page.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import LandingHighlights from "@/components/common/landingHighlights";
 import Button from "@/components/ui/button";
 import Image from 'next/image';
+import CircleShape from "@/assets/Ellipse.svg"
 export default function Landing() {
     return (
         <>
         <div className="grid h-screen grid-rows-[55%_45%]">
-            <div className="bg-(--color-primary)">
+            <div className="bg-(--color-primary) -z-10">
+                <div className="fixed inset-0 -z-10 overflow-hidden">
+                    <Image src={CircleShape} alt="Circle Shape" className="absolute top-[-60%] left-[-50%] w-[100%] h-[100%] " />   
+                    <Image src={CircleShape} alt="Circle Shape" className="absolute top-[30%] left-[0%] w-[60%] h-[60%] " />  
+                    <Image src={CircleShape} alt="Circle Shape" className="absolute top-[-30%] left-[23%] w-[80%] h-[80%] " /> 
+                    <Image src={CircleShape} alt="Circle Shape" className="absolute top-[30%] left-[65%] w-[60%] h-[60%] " /> 
+                </div>
                 <div className="flex flex-col w-full">
                     <div>
                         <div className="flex items-center gap-2 ml-10 mt-10">
@@ -17,20 +24,24 @@ export default function Landing() {
                         </div>
                     </div>
                     <div>
-
+                        <p className="font-bold text-white text-7xl ml-10 mt-15">
+                            Discover the future of digital forensics
+                        </p>
                     </div>
                     <div>
-
+                        <p className="text-(--color-light) text-lg ml-10 mt-5">
+                            Transform your digital forensics workflow with our cutting-edge platform.
+                        </p>
                     </div>
                     <div>
-                        <div className="flex items-center ml-10 mt-10 gap-5">
-                            <Button text="Sign Up" variant="submit" className="px-20"/>
-                            <Button text="Log In" variant="light" className="px-20"/>
+                        <div className="flex items-center ml-10 mt-25 gap-6">
+                            <Button text="Sign Up" variant="submit" className="px-20 py-3 text-lg font-semibold"/>
+                            <Button text="Log In" variant="light" className="px-20 py-3 text-lg font-semibold"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 z-20 bg-white">
                 <div className="flex items-center gap-2 ml-10 mt-5">
                     <div>
                         <p>FEATURE HIGHLIGHTS</p>
