fb2a58bed56d5a10aec07ff54d56fb6cabd09f61
Mulondi-25 <makhadomulondi@gmail.com>
2026-07-04 02:28:18 +0200

    Created landing page

---
+++ frontend/src/app/(auth)/landing/page.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import LandingHighlights from "@/components/common/landingHighlights";
+import Button from "@/components/ui/button";
+import Image from 'next/image';
+export default function Landing() {
+    return (
+        <>
+        <div className="grid h-screen grid-rows-[55%_45%]">
+            <div className="bg-(--color-primary)">
+                <div className="flex flex-col w-full">
+                    <div>
+                        <div className="flex items-center gap-2 ml-10 mt-10">
+                            <Image src="/VL_Logo_light.svg" alt="Veritas Lab Logo" width={80} height={80} className="" />
+                            <div className="font-semibold text-5xl ml-5 text-white">
+                                Veritas Lab
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+                        <div className="flex flex-col items-center justify-center mt-10">
+                            <Button />
+                            <Button />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1">
+                <div className="flex items-center gap-2 ml-10 mt-5">
+                    <div>
+                        <p>FEATURE HIGHLIGHTS</p>
+                    </div>
+                    <div>
+                        <LandingHighlights />
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
