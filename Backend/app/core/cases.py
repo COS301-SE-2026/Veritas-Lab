29500e7e7cad4c1f5f972db8c54199408e4a9d8d
BPillay27 <byronpillay2704@gmail.com>
2026-07-07 12:58:43 +0200

    File Creation and Indentation fixes

--- Backend/app/core/cases.py
+++ Backend/app/core/cases.py
@@ -670,10 +670,10 @@ class Case:
 
                     if deleted_media is not None:
                         orphan_media.append({
-                                "mediaid": deleted_media["mediaid"],
-                                "mediabucket": deleted_media["mediabucket"],
-                                "mediaextension": deleted_media["mediaextension"]
-                            })                 
+                            "mediaid": deleted_media["mediaid"],
+                            "mediabucket": deleted_media["mediabucket"],
+                            "mediaextension": deleted_media["mediaextension"]
+                        })                 
                 
             minioEndpointRaw = (
                 os.getenv("MINIO_ENDPOINT")
