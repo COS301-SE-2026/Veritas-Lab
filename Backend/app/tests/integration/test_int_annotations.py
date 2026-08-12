61a6cc33083122949dcc4e393d8d9733c472cf6d
Keegan-l-star <u24594522@tuks.co.za>
2026-08-12 16:00:32 +0200

    Finished up the tests

--- Backend/app/tests/integration/test_int_annotations.py
+++ Backend/app/tests/integration/test_int_annotations.py
@@ -678,4 +678,55 @@ async def test_integration_create_comment_lack_perm(client,fake_comment_context)
     assert response.json()["detail"]["message"] == "Permission denied"
     await assert_against_comment_table(user_name)
 
+@pytest.mark.asyncio
+async def test_integration_create_comment_lack_perm(client,fake_comment_context):
+    case_id=fake_comment_context["case_id1"]
+    user_name="MRBeast"
+    mock_invest = mock_invest = {
+        "id": "9b74b4e3-7823-464b-a65f-4df2d75eeab3",
+        "username": user_name,
+        "role": "USER"
+    }
+
+    client.cookies.set(COOKIE_NAME, create_token(mock_invest))
+    comment="Good day"
+    payload={
+        "case_id": case_id,
+        "comment": comment
+    }
+    response = client.post(
+        "/api/cases/comments",
+        json=payload
+    )
+
+    assert response.status_code == 403
+    assert response.json()["detail"]["status"] == "error"
+    assert response.json()["detail"]["message"] == "Users may only comment on closed cases"
+    await assert_against_comment_table(user_name)
+
 #404
+@pytest.mark.asyncio
+async def test_integration_create_comment_case_not_found(client,fake_comment_context):
+    case_id="2e067604-67c0-4b56-aeab-ca92e702aeb6"
+    user_name="MRBeast"
+    mock_invest = mock_invest = {
+        "id": "9b74b4e3-7823-464b-a65f-4df2d75eeab3",
+        "username": user_name,
+        "role": "INVESTIGATOR"
+    }
+
+    client.cookies.set(COOKIE_NAME, create_token(mock_invest))
+    comment="Good day"
+    payload={
+        "case_id": case_id,
+        "comment": comment
+    }
+    response = client.post(
+        "/api/cases/comments",
+        json=payload
+    )
+
+    assert response.status_code == 404
+    assert response.json()["detail"]["status"] == "error"
+    assert response.json()["detail"]["message"] == "Case not found"
+    await assert_against_comment_table(user_name)
