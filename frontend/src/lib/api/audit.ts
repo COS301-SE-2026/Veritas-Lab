19bc82f8f9fcaeda55fc3c6fc64f0e82ead54adf
Mulondi <makhadomulondi@gmail.com>
2026-08-28 14:50:21 +0200

    Added api call to fetch all audit's for the admin page

--- frontend/src/lib/api/audit.ts
+++ frontend/src/lib/api/audit.ts
@@ -17,3 +17,20 @@ export async function getAudit(caseID: string) {
     }
     return data;
 }
+
+export async function getAllAudit() {
+    const res = await fetch(`/api/getAllAudit`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    
+    const data = await res.json().catch(() => null);
+    if(!res.ok) {
+        const error = data as ApiError | null;
+        throw new Error(error?.detail?.message || 'Failed to fetch all audits');
+    }
+    return data;
+}
