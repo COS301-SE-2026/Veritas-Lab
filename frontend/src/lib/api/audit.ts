af20c2b47883cc13a4898d6fe8ecdcb3967c8a76
Mulondi <makhadomulondi@gmail.com>
2026-08-28 14:33:45 +0200

    Added get audit api call

---
+++ frontend/src/lib/api/audit.ts
@@ -0,0 +1,19 @@
+import { ApiError } from "@/types/api";
+
+export async function getAudit(caseID: string) {
+    const res = await fetch(`/api/getAudit`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ CaseID: caseID })
+    })
+    
+    const data = await res.json().catch(() => null);
+    if(!res.ok) {
+        const error = data as ApiError | null;
+        throw new Error(error?.detail?.message || 'Failed to fetch audit');
+    }
+    return data;
+}
