f4060b1b4999f2e76208c14e313b10ae277a9a96
Keegan-l-star <u24594522@tuks.co.za>
2026-09-03 19:47:02 +0200

    Added the .

--- Backend/app/database/init_cases.sql
+++ Backend/app/database/init_cases.sql
@@ -5,6 +5,6 @@ INSERT INTO "Cases_DB"."MediaType" (MediaName,MediaBucket, MediaExtension) VALUE
 ('Joint Photographic Experts Group','images', '.jpeg'),
 ('JPEG Image','images', '.jpg'),
 ('Portable Document Format','pdf', '.pdf'),
-('Multipart version 4','videos', 'mp4')
+('Multipart version 4','videos', '.mp4')
 ON CONFLICT (MediaName) DO NOTHING;
 
