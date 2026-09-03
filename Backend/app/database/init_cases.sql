75a9bc5d9e5c8caad2108a1e5f09bc222d6cfe5a
Keegan-l-star <u24594522@tuks.co.za>
2026-09-03 18:25:14 +0200

    This is to allow the videos to be uploaded

--- Backend/app/database/init_cases.sql
+++ Backend/app/database/init_cases.sql
@@ -4,6 +4,7 @@ INSERT INTO "Cases_DB"."MediaType" (MediaName,MediaBucket, MediaExtension) VALUE
 ('Portable Network Graphics','images', '.png'),
 ('Joint Photographic Experts Group','images', '.jpeg'),
 ('JPEG Image','images', '.jpg'),
-('Portable Document Format','pdf', '.pdf')
+('Portable Document Format','pdf', '.pdf'),
+('Multipart version 4','videos', 'mp4')
 ON CONFLICT (MediaName) DO NOTHING;
 
