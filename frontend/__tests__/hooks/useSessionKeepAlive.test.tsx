e617f2ca1e59179ca55c21909b9eec31f5d181f9
Mulondi <makhadomulondi@gmail.com>
2026-09-10 19:19:08 +0200

    Added session window test

--- frontend/__tests__/hooks/useSessionKeepAlive.test.tsx
+++ frontend/__tests__/hooks/useSessionKeepAlive.test.tsx
@@ -20,7 +20,7 @@ describe('useSessionKeepAlive', () => {
         jest.useRealTimers();
     });
 
-    test('trigger refreshSession on window events', () => {
+    it('trigger refreshSession on window events', () => {
         renderHook(() => useSessionKeepAlive());
 
         window.dispatchEvent(new Event('click'));
@@ -31,6 +31,23 @@ describe('useSessionKeepAlive', () => {
         expect(refreshSession).toHaveBeenCalledTimes(1);
         window.dispatchEvent(new Event('mousemove'));
         expect(refreshSession).toHaveBeenCalledTimes(1);
+    })
+
+    it('refreshSession is not called again within the threshold', () => {
+        renderHook(() => useSessionKeepAlive());
+        
+        window.dispatchEvent(new Event('keydown'));
+        expect(refreshSession).toHaveBeenCalledTimes(1);
+        // 4 min in, so below the 5 min threshold therefore refreshSession should still be called a total of 1 times
+        jest.setSystemTime(time + (4*60*1000))
+        window.dispatchEvent(new Event('keydown'));
+        expect(refreshSession).toHaveBeenCalledTimes(1);
+
+        // Past 5 min so it should make the call
+        jest.setSystemTime(time + (5*60*1001))
+        window.dispatchEvent(new Event('keydown'));
+        expect(refreshSession).toHaveBeenCalledTimes(2);
+
     })
 })
 
