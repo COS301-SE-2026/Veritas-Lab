29b91b73eb93006421f0f8216ccbe80d36e6cf21
ArranTuksUni <u23655675@tuks.co.za>
2026-09-22 17:54:19 +0200

    updated api response for new buttons

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -21,11 +21,15 @@ export type ChangePasswordResponse = {
     message: string;
 };
 
+export type CaseState = 'OPEN' | 'PUBLISHED' | 'CLOSED';
+
 export type DashboardCase = {
     caseId: string;
     caseReviews: Record<string, unknown> | null;
     caseName: string;
     caseCreator: string;
+    caseState: CaseState;
+    caseAssigned: string | null;
     caseClosed: boolean;
     caseCreationDate: string;
 };
@@ -72,6 +76,8 @@ export type CaseResponse = {
         caseCreator: string;
         caseReviews: Record<string, unknown> | null;
         caseDescription: string | null;
+        caseState: CaseState;
+        caseAssigned: string | null;
         caseClosed: boolean;
         caseCreationDate: string | null;
     };
