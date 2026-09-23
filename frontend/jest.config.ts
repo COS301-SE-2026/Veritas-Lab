0539015fbe0c1d390694ddfc5ed55f5f2291c294
Mulondi <makhadomulondi@gmail.com>
2026-09-23 17:06:37 +0200

    Fixed workbench integration and sliderbar tests

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -101,7 +101,9 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
       '^@/(.*)$': '<rootDir>/src/$1',
-    },
+      '^@preact/signals-core$':
+      '<rootDir>/node_modules/@preact/signals-core/dist/signals-core.js',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom'
+
+class MockObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+    takeRecords() { return []; }
+}
+
+if (!global.ResizeObserver) {
+    global.ResizeObserver = MockObserver as unknown as typeof ResizeObserver;
+}
+
+if (!global.IntersectionObserver) {
+    global.IntersectionObserver = MockObserver as unknown as typeof IntersectionObserver;
+}
